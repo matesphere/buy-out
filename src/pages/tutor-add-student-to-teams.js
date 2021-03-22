5b0bf253b18c37708fe01cc82465c09e59d6f1d3
stuart.hull <stuart.hull@autotrader.co.uk>
2021-03-22 12:24:29 +0000

    Add students

--- src/pages/tutor-add-student-to-teams.js
+++ src/pages/tutor-add-student-to-teams.js
@@ -39,115 +39,99 @@ const IndexPage = () => {
                 <h2 className="sm-type-drum sm-type-drum--medium mt-4">Add student to teams</h2>
 
                 <form className="login-student-form mb-4" id="form-login">
-
                     <div className="side-grey row mb-4">
-                        <div className="col-lg-6 mb-2">
+                        <div className="col-lg-12 mb-2">
                             <p className="sm-type-guitar sm-type-guitar--medium">John Doe.</p>
-                        </div>
-                        <div className="col-lg-6 mb-2">
-                            <p className="form-label sm-type-amp">Teams</p>
-                            <div className="multiple-choice">
-                                <input className="form-control" id="id1" type="radio" name="team" />
-                                <label className="form-label" htmlFor="id1"><span className="yellowdot"></span> Yellow</label>
-                            </div>
-                            <div className="multiple-choice">
-                                <input className="form-control" id="id2" type="radio" name="team" />
-                                <label className="form-label" htmlFor="id2"><span className="reddot"></span> Red</label>
-                            </div>
-                            <div className="multiple-choice">
-                                <input className="form-control" id="id4" type="radio" name="team" />
-                                <label className="form-label" htmlFor="id4"><span className="tealdot"></span> Teal</label>
-                            </div>
-                            <div className="multiple-choice">
-                                <input className="form-control" id="id5" type="radio" name="team" />
-                                <label className="form-label" htmlFor="id5"><span className="purpledot"></span> Purple</label>
+                            <div className="team-select">
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="id1" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="id1">Yellow</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="id2" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="id2">Red</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="id4" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="id4">Teal</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="id5" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="id5">Purple</label>
+                                </div>
                             </div>
                         </div>
                     </div>
 
                     <div className="side-grey row mb-4">
-                        <div className="col-lg-6 mb-2">
+                        <div className="col-lg-12 mb-2">
                             <p className="sm-type-guitar sm-type-guitar--medium">John Doe.</p>
-                        </div>
-                        <div className="col-lg-6 mb-2">
-                            <p className="form-label sm-type-amp">Teams</p>
-                            <div className="multiple-choice">
-                                <input className="form-control" id="id1" type="radio" name="team" />
-                                <label className="form-label" htmlFor="id1"><span className="yellowdot"></span> Yellow</label>
-                            </div>
-                            <div className="multiple-choice">
-                                <input className="form-control" id="id2" type="radio" name="team" />
-                                <label className="form-label" htmlFor="id2"><span className="reddot"></span> Red</label>
-                            </div>
-                            <div className="multiple-choice">
-                                <input className="form-control" id="id4" type="radio" name="team" />
-                                <label className="form-label" htmlFor="id4"><span className="tealdot"></span> Teal</label>
-                            </div>
-                            <div className="multiple-choice">
-                                <input className="form-control" id="id5" type="radio" name="team" />
-                                <label className="form-label" htmlFor="id5"><span className="purpledot"></span> Purple</label>
+                            <div className="team-select">
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="id1" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="id1">Yellow</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="id2" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="id2">Red</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="id4" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="id4">Teal</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="id5" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="id5">Purple</label>
+                                </div>
                             </div>
                         </div>
                     </div>
 
                     <div className="side-grey row mb-4">
-                        <div className="col-lg-6 mb-2">
-                            <p className="sm-type-guitar sm-type-guitar--medium">John Doe.</p>
-                        </div>
-                        <div className="col-lg-6 mb-2">
-                            <p className="form-label sm-type-amp">Teams</p>
-                            <div className="multiple-choice">
-                                <input className="form-control" id="id1" type="radio" name="team" />
-                                <label className="form-label" htmlFor="id1"><span className="yellowdot"></span> Yellow</label>
-                            </div>
-                            <div className="multiple-choice">
-                                <input className="form-control" id="id2" type="radio" name="team" />
-                                <label className="form-label" htmlFor="id2"><span className="reddot"></span> Red</label>
-                            </div>
-                            <div className="multiple-choice">
-                                <input className="form-control" id="id4" type="radio" name="team" />
-                                <label className="form-label" htmlFor="id4"><span className="tealdot"></span> Teal</label>
-                            </div>
-                            <div className="multiple-choice">
-                                <input className="form-control" id="id5" type="radio" name="team" />
-                                <label className="form-label" htmlFor="id5"><span className="purpledot"></span> Purple</label>
+                        <div className="col-lg-12 mb-2">
+                            <p className="sm-type-guitar sm-type-guitar--medium">Jane Doe.</p>
+                            <div className="team-select">
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="id1" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="id1">Yellow</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="id2" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="id2">Red</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="id4" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="id4">Teal</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="id5" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="id5">Purple</label>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                  <button className="btn-solid-lg" to="/introduction">Save</button>
+
+
                 </form>
 
               </div>
               <div className="col-lg-4">
                   <p className="sm-type-guitar mb-2"><span className="side-icon side-icon-orange"><HelpIcon /></span>Teams</p>
                   <div className="side-grey">
-                      <p className="sm-type-guitar sm-type-guitar--medium"><span className="yellowdot"></span> Team Yellow.</p>
-                      <p className="sm-type-lead">John Doe <span className="cross-icon"><Cross /></span></p>
-                      <p className="sm-type-lead">Jane Doe <span className="cross-icon"><Cross /></span></p>
-                      <p className="sm-type-guitar sm-type-guitar--medium mt-4"><span className="tealdot"></span> Team Teal.</p>
-                      <p className="sm-type-lead">John Doe <span className="cross-icon"><Cross /></span></p>
-                      <p className="sm-type-guitar sm-type-guitar--medium mt-4"><span className="reddot"></span> Team Red.</p>
-                      <p className="sm-type-lead">John Doe <span className="cross-icon"><Cross /></span></p>
-                      <p className="sm-type-lead">Jane Doe <span className="cross-icon"><Cross /></span></p>
-                      <p className="sm-type-guitar sm-type-guitar--medium mt-4"><span className="purpledot"></span> Team Purple.</p>
-                      <p className="sm-type-lead"> John Doe <span className="cross-icon"><Cross /></span></p>
-                      <p className="sm-type-lead">Jane Doe <span className="cross-icon"><Cross /></span></p>
+                      <p className="sm-type-leadguitar sm-type-lead--medium"><span className="yellowdot"></span> Team Yellow.</p>
+                      <p className="sm-type-amp">John Doe <span className="cross-icon"><Cross /></span></p>
+                      <p className="sm-type-amp">Jane Doe <span className="cross-icon"><Cross /></span></p>
+                      <p className="sm-type-lead sm-type-lead--medium mt-4"><span className="tealdot"></span> Team Teal.</p>
+                      <p className="sm-type-amp">John Doe <span className="cross-icon"><Cross /></span></p>
+                      <p className="sm-type-lead sm-type-lead--medium mt-4"><span className="reddot"></span> Team Red.</p>
+                      <p className="sm-type-amp">John Doe <span className="cross-icon"><Cross /></span></p>
+                      <p className="sm-type-amp">Jane Doe <span className="cross-icon"><Cross /></span></p>
+                      <p className="sm-type-lead-guitar sm-type-lead--medium mt-4"><span className="purpledot"></span> Team Purple.</p>
+                      <p className="sm-type-amp"> John Doe <span className="cross-icon"><Cross /></span></p>
+                      <p className="sm-type-amp">Jane Doe <span className="cross-icon"><Cross /></span></p>
                   </div>
-
-                  <p className="sm-type-guitar mb-2 mt-4"><span className="side-icon side-icon-orange"><HelpIcon /></span>Students unassigned</p>
-                  <div className="side-grey">
-                      <p className="sm-type-amp">John Doe</p>
-                      <p className="sm-type-amp">Jane Doe</p>
-                      <p className="sm-type-amp">John Doe</p>
-                      <p className="sm-type-amp">John Doe</p>
-                      <p className="sm-type-amp">Jane Doe</p>
-                      <p className="sm-type-amp">John Doe</p>
-                      <p className="sm-type-amp">Jane Doe</p>
-                      <p className="sm-type-amp">John Doe</p>
-                      <p className="sm-type-amp">John Doe</p>
-                      <p className="sm-type-amp">Jane Doe</p>
-                  </div>
+                  <button className="btn-solid-lg mt-4" to="/introduction">Save teams</button>
               </div>
             </div>
           </section>
