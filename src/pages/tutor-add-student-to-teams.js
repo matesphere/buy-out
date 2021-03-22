d5d2d467e8b6085a6fbe85438ea00c7cee035071
stuart.hull <stuart.hull@autotrader.co.uk>
2021-03-22 12:37:29 +0000

    Add students

--- src/pages/tutor-add-student-to-teams.js
+++ src/pages/tutor-add-student-to-teams.js
@@ -44,44 +44,32 @@ const IndexPage = () => {
                             <p className="sm-type-guitar sm-type-guitar--medium">John Doe.</p>
                             <div className="team-select">
                                 <div className="multiple-choice">
-                                    <input className="form-control" id="id1" type="radio" name="team" />
-                                    <label className="form-label" htmlFor="id1">Yellow</label>
+                                    <input className="form-control" id="11" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="11">Yellow</label>
                                 </div>
                                 <div className="multiple-choice">
-                                    <input className="form-control" id="id2" type="radio" name="team" />
-                                    <label className="form-label" htmlFor="id2">Red</label>
+                                    <input className="form-control" id="22" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="22">Red</label>
                                 </div>
                                 <div className="multiple-choice">
-                                    <input className="form-control" id="id4" type="radio" name="team" />
-                                    <label className="form-label" htmlFor="id4">Teal</label>
+                                    <input className="form-control" id="33" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="33">Teal</label>
                                 </div>
                                 <div className="multiple-choice">
-                                    <input className="form-control" id="id5" type="radio" name="team" />
-                                    <label className="form-label" htmlFor="id5">Purple</label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="side-grey row mb-4">
-                        <div className="col-lg-12 mb-2">
-                            <p className="sm-type-guitar sm-type-guitar--medium">John Doe.</p>
-                            <div className="team-select">
-                                <div className="multiple-choice">
-                                    <input className="form-control" id="id1" type="radio" name="team" />
-                                    <label className="form-label" htmlFor="id1">Yellow</label>
+                                    <input className="form-control" id="44" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="44">Purple</label>
                                 </div>
                                 <div className="multiple-choice">
-                                    <input className="form-control" id="id2" type="radio" name="team" />
-                                    <label className="form-label" htmlFor="id2">Red</label>
+                                    <input className="form-control" id="55" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="55">Brown</label>
                                 </div>
                                 <div className="multiple-choice">
-                                    <input className="form-control" id="id4" type="radio" name="team" />
-                                    <label className="form-label" htmlFor="id4">Teal</label>
+                                    <input className="form-control" id="66" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="66">Orange</label>
                                 </div>
                                 <div className="multiple-choice">
-                                    <input className="form-control" id="id5" type="radio" name="team" />
-                                    <label className="form-label" htmlFor="id5">Purple</label>
+                                    <input className="form-control" id="77" type="radio" name="team" />
+                                    <label className="form-label" htmlFor="77">Blue</label>
                                 </div>
                             </div>
                         </div>
@@ -92,26 +80,48 @@ const IndexPage = () => {
                             <p className="sm-type-guitar sm-type-guitar--medium">Jane Doe.</p>
                             <div className="team-select">
                                 <div className="multiple-choice">
-                                    <input className="form-control" id="id1" type="radio" name="team" />
-                                    <label className="form-label" htmlFor="id1">Yellow</label>
+                                    <input className="form-control" id="1" type="radio" name="team2" />
+                                    <label className="form-label" htmlFor="1">Yellow</label>
                                 </div>
                                 <div className="multiple-choice">
-                                    <input className="form-control" id="id2" type="radio" name="team" />
-                                    <label className="form-label" htmlFor="id2">Red</label>
+                                    <input className="form-control" id="2" type="radio" name="team2" />
+                                    <label className="form-label" htmlFor="2">Red</label>
                                 </div>
                                 <div className="multiple-choice">
-                                    <input className="form-control" id="id4" type="radio" name="team" />
-                                    <label className="form-label" htmlFor="id4">Teal</label>
+                                    <input className="form-control" id="3" type="radio" name="team2" />
+                                    <label className="form-label" htmlFor="3">Teal</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="4" type="radio" name="team2" />
+                                    <label className="form-label" htmlFor="4">Purple</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="5" type="radio" name="team2" />
+                                    <label className="form-label" htmlFor="5">Brown</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="6" type="radio" name="team2" />
+                                    <label className="form-label" htmlFor="6">Orange</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="7" type="radio" name="team2" />
+                                    <label className="form-label" htmlFor="7">Pink</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="8" type="radio" name="team2" />
+                                    <label className="form-label" htmlFor="8">Beige</label>
+                                </div>
+                                <div className="multiple-choice">
+                                    <input className="form-control" id="9" type="radio" name="team2" />
+                                    <label className="form-label" htmlFor="9">Gold</label>
                                 </div>
                                 <div className="multiple-choice">
                                     <input className="form-control" id="id5" type="radio" name="team" />
-                                    <label className="form-label" htmlFor="id5">Purple</label>
+                                    <label className="form-label" htmlFor="q2">Silver</label>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-
 
                 </form>
 
