09fc8baa6f46451693f4e7cef851b2c14148b080
stuart.hull <stuart.hull@autotrader.co.uk>
2021-03-23 16:47:50 +0000

    Add student hub

--- src/components/_footer.js
+++ src/components/_footer.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 
 import '../scss/index.scss'
+import {Link} from "gatsby";
 const Footer = () => {
     return (
         <footer className="footer">
@@ -8,8 +9,8 @@ const Footer = () => {
                 <div className="row">
                     <div className="col-lg-12">
                         <ul>
-                            <li>Home</li>
-                            <li>Team hub</li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/the-quest/">Team hub</Link></li>
                         </ul>
 
                         <p>Copyright 2021. All rights reserved.</p>
