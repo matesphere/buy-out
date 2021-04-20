4e2286eb2688604429eea50acfa605d37853f46a
David Carter <dccarta@gmail.com>
2021-04-20 10:36:53 +0100

    add DB reset button in tutor footer

--- src/components/_footer.js
+++ src/components/_footer.js
@@ -1,28 +1,26 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import '../scss/index.scss'
-import { Link } from 'gatsby'
-const Footer = () => {
-    return (
-        <footer className="footer">
-            <section className="container">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/student/team-hub/">Team hub</Link>
-                            </li>
-                        </ul>
 
-                        <p>Copyright 2021. All rights reserved.</p>
-                    </div>
+const Footer = () => (
+    <footer className="footer">
+        <section className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/student/team-hub/">Team hub</Link>
+                        </li>
+                    </ul>
+
+                    <p>Copyright 2021. All rights reserved.</p>
                 </div>
-            </section>
-        </footer>
-    )
-}
-
+            </div>
+        </section>
+    </footer>
+)
 export default Footer
--- src/gql/mutations.js
+++ src/gql/mutations.js
@@ -167,3 +167,21 @@ export const MARK_PASSED = gql`
         }
     }
 `
+
+// TODO remove this from prod!!!!!
+export const RESET_DB = gql`
+    mutation ResetDB {
+        delete_team(where: {}) {
+            returning {
+                id
+            }
+        }
+        delete_user(
+            where: { id: { _neq: "8163dda8-eac2-4997-86e7-bb8addc37c3b" } }
+        ) {
+            returning {
+                id
+            }
+        }
+    }
+`
