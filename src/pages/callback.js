9880fde40aef12d7fa5a9fc69025a94320742c7c
cart <carter@soundmate.io>
2021-05-11 09:21:12 +0100

    remove unused auth stuff

--- src/pages/callback.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import { withAuth } from '../utils/auth/withAuth'
-
-const CallbackRoute = (props) => <Redirect to="/" />
-
-export default withAuth(CallbackRoute)
--- src/utils/auth/awsSDK.js
+++
@@ -1,7 +0,0 @@
-import { config as AWSConfig, Lambda } from 'aws-sdk'
-
-const AWSRegion = process.env.REACT_APP_AWS_REGION
-
-AWSConfig.region = AWSRegion
-
-export { AWSRegion, AWSConfig, Lambda }
