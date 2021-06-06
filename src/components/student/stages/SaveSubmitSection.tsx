343dda4ed8315da1293161e0405591594942b65a
stuart.hull <stuart.hull@autotrader.co.uk>
2021-06-06 13:35:21 +0100

    chore: Add modal for saved and submitted

--- src/components/student/stages/SaveSubmitSection.tsx
+++ src/components/student/stages/SaveSubmitSection.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from 'react'
+import React, {useState, FC, useContext} from 'react'
 import { MutationResult } from '@apollo/client'
 
 import SaveIcon from '../../../assets/save-icon.svg'
+import {NewQuestContext} from "../../../pages/tutor";
 interface SaveSubmitSectionProps {
     saveWorkObj?: {
         call: () => Promise<any>
@@ -13,6 +14,7 @@ interface SaveSubmitSectionProps {
     }
     disableSubmit: boolean
     submitted: boolean
+    showModal: boolean
 }
 
 // TODO bring in confirm modals
@@ -21,32 +23,70 @@ export const SaveSubmitSection: FC<SaveSubmitSectionProps> = ({
     submitWorkObj,
     disableSubmit,
     submitted,
-}) => (
-    <>
-        {!submitWorkObj.response.data && (
-            <>
-                {!!saveWorkObj && (
+}) => {
+    const [showModal, setShowModal] = useState(true)
+
+    return (
+        <>
+            {!submitWorkObj.response.data && (
+                <>
+                    {!!saveWorkObj && (
+                        <button
+                            className="btn-outline-lg mt-4 btn-icon"
+                            onClick={saveWorkObj.call}
+                        >
+                            <SaveIcon />
+                            Save Work
+                        </button>
+                    )}
+
                     <button
-                        className="btn-outline-lg mt-4 btn-icon"
-                        onClick={saveWorkObj.call}
+                        className="btn-solid-lg mt-4"
+                        disabled={disableSubmit}
+                        onClick={submitWorkObj.call}
                     >
-                        <SaveIcon />
-                        Save Work
+                        Submit Work
                     </button>
-                )}
+                </>
+            )}
 
-                <button
-                    className="btn-solid-lg mt-4"
-                    disabled={disableSubmit}
-                    onClick={submitWorkObj.call}
-                >
-                    Submit Work
-                </button>
-            </>
-        )}
+            {saveWorkObj.response.data && (
+                <>
+                    {showModal && (
+                        <div className="modal-window">
+                            <div>
+                                <button
+                                    onClick={() => setShowModal(false)}
+                                    title="Close"
+                                    className="modal-close"
+                                >
+                                    Close
+                                </button>
+                                <p className="sm-type-drum">Work saved.</p>
+                            </div>
+                        </div>
+                    )}
+                </>
+            )}
 
-        {submitWorkObj.response.data && (
-            <span>Work submitted - good luck!</span>
-        )}
-    </>
-)
+            {submitWorkObj.response.data && (
+                <>
+                    {showModal && (
+                        <div className="modal-window">
+                            <div>
+                                <button
+                                    onClick={() => setShowModal(false)}
+                                    title="Close"
+                                    className="modal-close"
+                                >
+                                    Close
+                                </button>
+                                <p className="sm-type-drum">Work submitted - good luck!</p>
+                            </div>
+                        </div>
+                    )}
+                </>
+            )}
+        </>
+    )
+}
