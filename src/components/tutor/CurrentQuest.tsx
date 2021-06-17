1245325605e0aed8671aaf87ee88f1ebd80eb1b0
stuart.hull <stuart.hull@autotrader.co.uk>
2021-06-17 16:00:13 +0100

    chore: Styling model answers

--- src/components/tutor/CurrentQuest.tsx
+++ src/components/tutor/CurrentQuest.tsx
@@ -93,45 +93,48 @@ export const UnlockedStage3Status = ({ devOptions, doc }) => {
             <Progress />
             <span>Unlocked</span>
 
-            <label className="form-label sm-type-amp">Model answers: </label>
-            <select
-                className="form-control"
-                value={devOption}
-                onChange={({ target: { value } }) => setDevOption(value)}
-            >
-                <option value="" defaultChecked>
-                    Select
-                </option>
-                {devOptions.map(({ option, display_name }, i) => (
-                    <option key={i} value={option}>
-                        {display_name}
-                    </option>
-                ))}
-            </select>
-            <button
-                onClick={() => {
-                    saveWork({
-                        variables: {
-                            docId: doc.id,
-                            docData: {
-                                ...doc.doc_data,
-                                [devOption]: {
-                                    ...devOptions.find(
-                                        (opt) => opt.option === devOption
-                                    ).model_swot,
-                                    provided: true,
-                                },
-                            },
-                        },
-                    })
-                }}
-            >
-                Provide
-            </button>
+            <p className="sm-type-lead sm-type-lead--medium mt-4">Provide Model Answers: </p>
 
             {providedAnswers.length > 0 && (
-                <p>Provided: {providedAnswers.join(', ')}</p>
+                <p className="sm-type-amp">Provided: <span className="redorange-highlight">{providedAnswers.join(', ')}</span></p>
             )}
+            <div className="model-answers">
+                <select
+                    className="form-control"
+                    value={devOption}
+                    onChange={({ target: { value } }) => setDevOption(value)}
+                >
+                    <option value="" defaultChecked>
+                        Select
+                    </option>
+                    {devOptions.map(({ option, display_name }, i) => (
+                        <option key={i} value={option}>
+                            {display_name}
+                        </option>
+                    ))}
+                </select>
+                <button
+                    className="btn-outline-reg"
+                    onClick={() => {
+                        saveWork({
+                            variables: {
+                                docId: doc.id,
+                                docData: {
+                                    ...doc.doc_data,
+                                    [devOption]: {
+                                        ...devOptions.find(
+                                            (opt) => opt.option === devOption
+                                        ).model_swot,
+                                        provided: true,
+                                    },
+                                },
+                            },
+                        })
+                    }}
+                >
+                    Provide
+                </button>
+            </div>
         </div>
     )
 }
