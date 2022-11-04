e09dcecbd1fbc08d42e8cc593fbd8c989a167a93
David Carter <dccarta@gmail.com>
2022-11-04 12:27:44 +0000

    provide tutor hub link on student footer where role is not student

--- src/components/_footer.tsx
+++ src/components/_footer.tsx
@@ -1,37 +1,50 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { AmplifySignOut } from '@aws-amplify/ui-react'
 import { Link } from 'gatsby'
 
+import { UserStateContext } from '../utils/user-state'
 import '../scss/index.scss'
 import Squiggle from '../assets/squiggle.svg'
 
-const Footer = () => (
-    <footer className="footer">
-        <Squiggle className="squiggle" />
-        <section className="container">
-            <div className="row">
-                <div className="col-lg-4 mt-1">
-                    <p>
-                        <Link to="/student/team-hub/">Team hub</Link>
-                        <br />
-                        <Link to="/acknowledgements">
-                            Creators and Acknowledgements
-                        </Link>
-                    </p>
+const Footer = () => {
+    const {
+        userInfo: { role },
+    } = useContext(UserStateContext)
+    return (
+        <footer className="footer">
+            <Squiggle className="squiggle" />
+            <section className="container">
+                <div className="row">
+                    <div className="col-lg-4 mt-1">
+                        <p>
+                            <Link to="/student/team-hub/">Team hub</Link>
+                            <br />
+                            <Link to="/acknowledgements">
+                                Creators and Acknowledgements
+                            </Link>
+                            {role !== 'student' && (
+                                <>
+                                    <br />
+                                    <Link to="/tutor/hub">Tutor Hub</Link>
+                                </>
+                            )}
+                        </p>
+                    </div>
+                    <div className="col-lg-4 mt-1"></div>
+                    <div className="col-lg-4">
+                        <AmplifySignOut />
+                    </div>
                 </div>
-                <div className="col-lg-4 mt-1">
-
+                <div className="row">
+                    <div className="col-lg-12 text-align-center mt-2">
+                        <p>
+                            &copy; Copyright MateSphere 2021. All rights
+                            reserved.
+                        </p>
+                    </div>
                 </div>
-                <div className="col-lg-4">
-                    <AmplifySignOut />
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-lg-12 text-align-center mt-2">
-                    <p>&copy; Copyright MateSphere 2021. All rights reserved.</p>
-                </div>
-            </div>
-        </section>
-    </footer>
-)
+            </section>
+        </footer>
+    )
+}
 export default Footer
